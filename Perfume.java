d5daed95aeb016ac4feb6534d7e800f8f57120ba
jeevanjchannapla2002 <jeevanjchannapla2002@gmail.com>
2024-06-21 11:34:26 +0530

    Ticket class

---
+++ Perfume.java
@@ -0,0 +1,21 @@
+class Perfume{
+
+	static int perfumeId = 37;
+	static String brandName = "FOGG";
+	static String mfgDate = "29/05/2024";
+	static String expDate = "23/06/2025";
+	static String perfumeType =("Universal/Women/Men");
+		
+	public static void main(String prefume[]){
+	
+		System.out.println("ID:" + perfumeId);
+		System.out.println("BRANDNAME:" + brandName);
+		System.out.println("Manufacture Date:" + mfgDate);
+		System.out.println("Expiry Date:" + expDate);
+		System.out.println("Type: " + perfumeType);
+	
+	
+		
+	
+	}
+}
---
+++ Ticket.java
@@ -0,0 +1,12 @@
+class Ticket{
+	public static void main(String ticket[]){
+		System.out.println("the ticket class");
+		
+	
+	}
+
+
+
+
+
+}
